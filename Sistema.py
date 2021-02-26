a6ede4c9c267a5aaf629208132612b6989585e87
MayconCoutinho <mayconcoutinhoart@gmail.com>
2021-02-26 09:23:11 -0300

    adding new files

--- Sistema.py
+++ Sistema.py
@@ -3,8 +3,54 @@ from time import sleep
 
 cor1 = '\33[1;32m' 
 cor0 = '\33[m'
+
+combate_imagem = ('''
+
+
+     ┌─┬─┐─┬─┐            
+     │▒│▒|▒│▒│           
+    ┌┴─┴─┐-┘─┘           
+    │▒┌──┘▒▒▒│           
+    └┐▒▒▒▒▒▒┌┘            
+    　└┐▒▒▒▒┌┘
+''',
+''' 
+
+      ┌─┌─┬─┐            
+      │▒│▒|▒│─┐          
+      │▒│▒|▒│▒│         
+      │▒│▒|▒│▒│          
+     ┌┴─┴─┐-┘─┘        
+     │▒┌──┘▒▒▒│          
+     └┐▒▒▒▒▒▒┌┘          
+      └┐▒▒▒▒┌┘            
+''' 
+,'''
+
+        ┌─┐　─┐
+        │▒│ /▒/
+        │▒│/▒/
+        │▒ /▒/─┬─┐ 
+        │▒│▒|▒│▒│
+        │▒┌──┘▒▒▒│
+        └┐▒▒▒▒▒▒┌┘
+          └┐▒▒▒▒┌
+
+''')
+
+vs = (''' 
+
+
+
+                 ⎯⎯      
+          ╲  /  │⎯⎯                
+           ╲/   ⎯⎯│            
+                
+
+''')
 def regras(): 
-    print(24*'                                                                                                                            ')
+    
+    print(22*'                                                                                                                            ')
     print(''' 
     Digite:
         -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -31,12 +77,45 @@ def regras():
         
         '''.format(cor0,cor1))
                                                                                             
+def combate():
 
+    c = computador-1
+    j = jogador-1
+    print(''' 
+    
+    MAQUINA: 
+    {1}
+
+    {2} 
+
+    JOGADOR:
+    {0}
+    
+    
+    
+    
+    
+    '''.format(combate_imagem[(j)],combate_imagem[(c)],vs))
+
+def resultado_imagem(): 
+    print('''
+    
+    
+    {0}
+    
+    
+    
+    
+    '''.format(resultado))
+
+    
+    
+    
 sair = False
 
-
-
 while sair != True:
+
+    sleep(4)
     regras() 
 
     jogadas = ['Pedra','Papel','Tesoura']
@@ -47,42 +126,53 @@ while sair != True:
     computador = random.randint(1,3) 
     confere_computador = jogadas[(computador-1)]
 
+    
+
 
     if jogador  == computador:
-        print('\33[1;33m Empate  \33[m')
-        print('computador jogou: {}'.format(confere_computador))
-        print('Jogador jogou: {}'.format(confere_jogador))
+        resultado = ('\33[1;33m Empate  \33[m')
+        combate()
+        sleep(3)
+        resultado_imagem()
 
     elif jogador == 3:
         if computador == 1:
-            print('\033[1;31m você perdeu!\033[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
-
+            resultado = ('\033[1;31m você perdeu!\033[m')
+            combate()
+            sleep(2)
+            resultado_imagem()
         elif computador == 2:
             print('\33[0;32m Você ganhou!\33[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
+            combate()
+            sleep(2)
+            resultado_imagem()
 
     elif jogador == 2:
         if computador == 1:
-            print('\33[0;32m Você ganhou!\33[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
+            resultado = ('\33[0;32m Você ganhou!\33[m')
+            combate()
+            sleep(2)
+            resultado_imagem()
 
         elif computador == 3:
-            print('\33[1;31m Você perdeu! \33[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
+            resultado = ('\33[1;31m Você perdeu! \33[m')
+            combate()
+            sleep(2)
+            resultado_imagem()
+
     elif jogador == 1:
         if computador == 2:
-            print('\33[1;31m Você perdeu! \33[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
+            resultado = ('\33[1;31m Você perdeu! \33[m')
+            combate()
+            sleep(2)
+            resultado_imagem()
+
         elif computador == 3:
-            print('\33[0;32m Você ganhou! \33[m')
-            print('Computador jogou: {}'.format(confere_computador))
-            print('Jogador jogou: {}'.format(confere_jogador))
+            resultado = ('\33[0;32m Você ganhou! \33[m')
+            combate()
+            sleep(2)
+            resultado_imagem()
+
     elif jogador == 0:
         print('Saindo ...')
         sleep(3)
